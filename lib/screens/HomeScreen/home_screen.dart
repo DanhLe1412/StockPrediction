3462a0f5865347fcd74514873864e9bda902394d
TruongQuyHai <haiquytruong@gmail.com>
2021-07-08 09:47:56 +0700

    Stock Row info

--- lib/screens/HomeScreen/home_screen.dart
+++ lib/screens/HomeScreen/home_screen.dart
@@ -1,6 +1,8 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:stock_prediction/screens/HomeScreen/widgets/header.dart';
 import 'package:stock_prediction/screens/HomeScreen/widgets/info_card.dart';
+import 'package:stock_prediction/screens/HomeScreen/widgets/row_stock.dart';
 import 'package:stock_prediction/screens/HomeScreen/widgets/search_box.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -22,60 +24,131 @@ class _HomeScreenState extends State<HomeScreen> {
         body: SafeArea(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
-            child: SingleChildScrollView(
-              child: ConstrainedBox(
-                constraints: BoxConstraints(
-                  maxHeight: MediaQuery.of(context).size.height,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Header(),
+                SizedBox(height: 32),
+                SearchBox(),
+                SizedBox(height: 32),
+                InfoCard(),
+                SizedBox(height: 32),
+                Text(
+                  'Trending',
+                  style: TextStyle(
+                    fontSize: 26,
+                    letterSpacing: 0.5,
+                  ),
                 ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Header(),
-                    SizedBox(height: 32),
-                    SearchBox(),
-                    SizedBox(height: 32),
-                    InfoCard(),
-                    SizedBox(height: 32),
-                    Text(
-                      'Trending',
-                      style: TextStyle(
-                        fontSize: 26,
-                        letterSpacing: 0.5,
-                      ),
-                    ),
-                    SizedBox(height: 24),
-                    Expanded(
-                      child: ListView(
-                        physics: NeverScrollableScrollPhysics(),
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    'sbux'.toUpperCase(),
-                                  ),
-                                  Text('Starbucks'),
-                                ],
-                              ),
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Text('780,64'),
-                                  Text('+0.20'),
-                                ],
-                              ),
-                            ],
-                          ),
+                SizedBox(height: 24),
+                Expanded(
+                  child: ListView(
+                    physics: NeverScrollableScrollPhysics(),
+                    children: [
+                      RowStock(
+                        stockId: 'sbux',
+                        brand: 'Starbucks',
+                        spots: [
+                          FlSpot(0, 40),
+                          FlSpot(10, 15),
+                          FlSpot(20, 35),
+                          FlSpot(30, 33),
+                          FlSpot(40, 55),
+                          FlSpot(50, 45),
+                          FlSpot(60, 55),
+                          FlSpot(67, 54),
+                          FlSpot(80, 80),
+                          FlSpot(90, 70),
+                          FlSpot(100, 82),
                         ],
+                        value: '780,64',
+                        changes: '+0.20',
                       ),
-                    ),
-                  ],
+                      SizedBox(height: 16),
+                      RowStock(
+                        stockId: 'aapl',
+                        brand: 'Apple Inc.',
+                        spots: [
+                          FlSpot(0, 40),
+                          FlSpot(10, 15),
+                          FlSpot(20, 35),
+                          FlSpot(30, 33),
+                          FlSpot(40, 55),
+                          FlSpot(50, 45),
+                          FlSpot(60, 55),
+                          FlSpot(67, 54),
+                          FlSpot(80, 80),
+                          FlSpot(90, 70),
+                          FlSpot(100, 82),
+                        ],
+                        value: '780,64',
+                        changes: '+0.20',
+                      ),
+                      SizedBox(height: 16),
+                      RowStock(
+                        stockId: 'aapl',
+                        brand: 'Apple Inc.',
+                        spots: [
+                          FlSpot(0, 40),
+                          FlSpot(10, 15),
+                          FlSpot(20, 35),
+                          FlSpot(30, 33),
+                          FlSpot(40, 55),
+                          FlSpot(50, 45),
+                          FlSpot(60, 55),
+                          FlSpot(67, 54),
+                          FlSpot(80, 80),
+                          FlSpot(90, 70),
+                          FlSpot(100, 82),
+                        ],
+                        value: '780,64',
+                        changes: '+0.20',
+                      ),
+                      SizedBox(height: 16),
+                      RowStock(
+                        stockId: 'aapl',
+                        brand: 'Apple Inc.',
+                        spots: [
+                          FlSpot(0, 40),
+                          FlSpot(10, 15),
+                          FlSpot(20, 35),
+                          FlSpot(30, 33),
+                          FlSpot(40, 55),
+                          FlSpot(50, 45),
+                          FlSpot(60, 55),
+                          FlSpot(67, 54),
+                          FlSpot(80, 80),
+                          FlSpot(90, 70),
+                          FlSpot(100, 82),
+                        ],
+                        value: '780,64',
+                        changes: '+0.20',
+                      ),
+                      SizedBox(height: 16),
+                      RowStock(
+                        stockId: 'aapl',
+                        brand: 'Apple Inc.',
+                        spots: [
+                          FlSpot(0, 40),
+                          FlSpot(10, 15),
+                          FlSpot(20, 35),
+                          FlSpot(30, 33),
+                          FlSpot(40, 55),
+                          FlSpot(50, 45),
+                          FlSpot(60, 55),
+                          FlSpot(67, 54),
+                          FlSpot(80, 80),
+                          FlSpot(90, 70),
+                          FlSpot(100, 82),
+                        ],
+                        value: '780,64',
+                        changes: '+0.20',
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+              ],
             ),
           ),
         ),
